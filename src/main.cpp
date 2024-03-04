a34779a98521b377c63c034d58b72c5cec7a039e
KIM Min Joong <minjoong38@gmail.com>
2024-03-04 17:14:28 +0800

    add PID

--- src/main.cpp
+++ src/main.cpp
@@ -1,345 +1,147 @@
-#include "calibration.h"
+#include <RS485.h>
+#include <TimerOne.h>
+#include <TimerTwo.h>
+#include <Arduino.h>
 
-#define sonic_0 A0
-#define sonic_1 A1
-#define tof A2
+#define R_usonic A0
+#define L_usonic A1
+#define L_TOF A2
 
 const uint8_t sendPin  = 8;
 const uint8_t deviceID = 0;
 RS485 rs485(&Serial1, sendPin);  //uses default deviceID
 
-// PID parameters
-const uint8_t Kp = 12;
-const uint8_t Ki = 0;
-const uint8_t Kd = 5;
-int P, I, D, prev_e = 0;
-int I_lat, D_lat, prev_e_lat = 0;
-float sample_t = 0.05;
-int tau = 2;
-int cnt_alg = 0;
-
-// lateral motion
-float current_pos_lat = 0;
-float desired_pos_lat = 0;
-
-// longitudinal motion
-float desired_pos_long; // fixed y position for scanning (300 mm)
-int current_pos_long = 0;
-int desired_pos_adc = 0;
-
-uint32_t lastCommand = 0; 
-uint8_t commandState, group = 2;
-
-unsigned long alg_timeout; // alignment timeout (10 seconds)
-
-// internal flags for traffic control
-bool alg = false;         // angular alignment 
-bool mv_lat = false;      // lateral motion 500 mm
-bool mv_long = false;     // longitudinal motion
-
-int sensor_0; // right ultrasonic
-int sensor_1; // left ultrasonic
-int sensor_2; // TOF 
-// longitudinal pos = (485/1023) * (sensor_0 + sensor_1) * 0.5 + 15
-// lateral pos = sensor_2 * (2350/1023) + 150;
-
-// RS485 commands
 byte idle[11]   = {0x01, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x87, 0x4A}; 
 byte fwd[11]    = {0x01, 0x06, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x97, 0x8A};
 byte bkwd[11]   = {0x01, 0x06, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xA4, 0x8A};
 byte ccw[11]    = {0x01, 0x06, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xC2, 0x8A};
 byte cw[11]     = {0x01, 0x06, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0E, 0x8A};
-byte slow[11]   = {0x01, 0x06, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xA6, 0x88};
-byte fast[11]   = {0x01, 0x06, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x96, 0x8B};
 byte left[11]   = {0x01, 0x06, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x76, 0x8A};
 byte right[11]  = {0x01, 0x06, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0xFE, 0x8A};
+  // 0.002 m/s increase or decrease speed for sending every 20 ms
+byte slow[11]   = {0x01, 0x06, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xA6, 0x88};
+byte fast[11]   = {0x01, 0x06, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x96, 0x8B};
 
-void callbackCommand();
-void setCommand(int incomingByte, int num = 0);
-void align(); // angular adjustment
-void move_lat();  // lateral motion
-void move_long(); // longitudinal motion
-void manual(); // manual control
-int PID();
+int cnt = 0; // interrupt counter to control speed
+int cmd_state = 0;
+unsigned long last_cmd_time = 0;
+
+float current_lat_speed = 0.1; // max: 0.1, min = 0.02
+float lat_pos = 0;
+float desired_lat_pos = 0;
+
+bool lateral_mode = false;
+
+// PID parameters
+float P, I, D;
+float Kp = 0.5;
+float Ki = 0.5;
+float Kd = 0.5;
+float tau = 0.5;
+float sample_t = 0.05;
+float prev_e = 0;;
+
+void cntrl();
+void cntrl_speed();
+void set_cmd(int incomingByte);
 
 void setup() {
   Serial.begin(115200);
   Serial1.begin(9600);
   while (!Serial);
   Timer1.initialize(50000); // 50 millseconds
-  Timer1.attachInterrupt(callbackCommand);
+  Timer1.attachInterrupt(cntrl); 
+  Timer2.init(20000, cntrl_speed); // 20 millseconds
 }
 
 void loop() {
-  if (Serial.available() > 0) {
+  if(Serial.available() > 0){
     int incomingByte = Serial.read();
-    if (incomingByte == 'N' || incomingByte == 'n') {
-      char num[4]; 
-      for(int i = 0; i < 4; i++) {
-        num[i] = 0;
-      }
-      Serial.readBytes(num,4);
-      num[3] = '\0';
-      setCommand(incomingByte, atoi(num));
+    if(incomingByte == 'c') {
+      lateral_mode = true;
+      desired_lat_pos = lat_pos + 500; // target set to move 500 mm to the right
     }
-    else {
-      setCommand(incomingByte);
-      }
+    set_cmd(incomingByte);
   }
 }
 
-void callbackCommand() {
-  sensor_0 = analogRead(sonic_0);
-  sensor_1 = analogRead(sonic_1);
-  sensor_2 = analogRead(tof);
-  current_pos_lat = sensor_2 * 2.297 + 150;
-  current_pos_long = ((sensor_0 + sensor_1) * 0.5);
-  // Serial.println(sensor_0);
-  // Serial.println(sensor_1);
-  // Serial.println(sensor_2);
-  // Serial.println("");
-
-  if (alg) {
-    align();
-  }
-  else if (mv_lat) {
-    move_lat();
-  }
-  else if (mv_long) {
-    move_long();
-  }
-  else {
-    manual();
-  }
-}
-
-void setCommand(int incomingByte, int num = 0) {
-  if (incomingByte == 32) { // space
-    commandState = 0;
-  } else if ((incomingByte == 87) || (incomingByte == 119)) { // W or w(forward)
-    commandState = 1;
-  } else if ((incomingByte == 83) || (incomingByte == 115)) { // S or s(backward)
-    commandState = 2;
-  } else if ((incomingByte == 81) || (incomingByte == 113)) { // Q or q(ccw)
-    commandState = 3;
-  } else if ((incomingByte == 69) || (incomingByte == 101)) { // E or e (cw)
-    commandState = 4;
-  } else if (incomingByte == 45) { // -(slower)
-    commandState = 5;
-  } else if (incomingByte == 43) { // +(faster)
-    commandState = 6;
-  } else if ((incomingByte == 65) || (incomingByte == 97)) { // A or a (move left)
-    commandState = 7;
-  } else if ((incomingByte == 68) || (incomingByte == 100)) { // D or d (move right)
-    commandState = 8;
-  } else if (incomingByte == 77 || incomingByte == 109) { // 'M' or 'm' for alignmnent
-    alg = true;
-  } else if (incomingByte == 67 || incomingByte == 99) {  // 'C(number)' or 'c' for move side TOF
-    desired_pos_lat = current_pos_lat + 500;
-    // Serial.println(desired_pos_lat);
-    mv_lat = true;
-  } else if (incomingByte == 78 || incomingByte == 110) { // 'N(number)' or 'n' for adjusting longitudinal position ULTRASONIC
-    desired_pos_long = num;
-    // Serial.println(desired_pos_long);
-    desired_pos_adc = (desired_pos_long - 15) / 0.474;
-    mv_long = true;
-  } else if (incomingByte == 80 || incomingByte == 112) { // 'P' or 'p' for sending sensor data
-    Serial.print(0.4741 * (sensor_0 + sensor_1) * 0.5 + 15);
-    Serial.print(" ");
-    Serial.print(sensor_2 * 2.2971 + 150);
-    Serial.print(" ");
-  }
-}
-
-void align() {
-  alg_timeout = millis();
-  if (abs(sensor_0 - sensor_1) >= 6) {
-      group = PID();
-      if (sensor_0 > sensor_1) {
-          for (int j = 0; j < group; j++) {
-            for (int i = 0; i < 11; i++) rs485.write(ccw[i]);
-        }
-        for (int i = 0; i < 11; i++) rs485.write(idle[i]);
-      }
-      else if (sensor_0 < sensor_1) {
-        for (int j = 0; j < group; j++) {
-          for (int i = 0; i < 11; i++) rs485.write(cw[i]);
-        }
-        for (int i = 0; i < 11; i++) rs485.write(idle[i]);
-      }
+void cntrl(){
+  int R_usonic_val = analogRead(R_usonic);
+  int L_usonic_val = analogRead(L_usonic);
+  int L_TOF_val = analogRead(L_TOF);
+  lat_pos = L_TOF_val * (2350/1023) + 150; // current lateral pos from the left wall in mm
+  if(lateral_mode) {
+    if (abs(desired_lat_pos - lat_pos) <= 5) {
+      lateral_mode = false;
     }
-  else {
-    ++cnt_alg;
-  }
-
-  if (cnt_alg >= 20 && abs(sensor_0 - sensor_1) <= 6) {
-    alg = false;
-    cnt_alg = 0;
-    group = 2;
-  }
-  else if (millis() - alg_timeout > 10000 && abs(sensor_0 - sensor_1) <= 10) {
-    alg = false;
-    cnt_alg = 0;
-    group = 2;
-  }
-}
-void move_lat() {
-  if (abs(current_pos_lat - desired_pos_lat) <= 5) {
-    mv_lat = false;
-  } else {
-    // Calculate error
-    int error = desired_pos_lat - current_pos_lat;
-
-    // Calculate PID terms
-    int P_lat = 5 * error;  // Kp = 5
-    I_lat += 0 * sample_t * (error + prev_e_lat) * 0.5; // Ki = 0
-    D_lat = ((2 * 0) / (sample_t + 2 * tau)) * (error - prev_e_lat) - (
-          (sample_t - 2 * tau) / (sample_t + 2 * tau)) * D_lat;  // Kd = 0
-
-    // Update previous error
-    prev_e_lat = error;
-
-    // Calculate control output
-    int control_output = P_lat + I_lat + D_lat;
-
-    // Anti-windup (optional)
-    if (control_output > 500) control_output = 500;
-    else if (control_output < -500) control_output = -500;
-
-    // Determine movement direction and send command
-    if (control_output > 0) {
-      // Send right command based on control output (e.g., adjust speed)
-      for (int j = 0; j < control_output / 100; j++) {
-        for (int i = 0; i < 11; i++) rs485.write(right[i]);
-      }
-    } else if (control_output < 0) {
-      // Send left command based on control output (e.g., adjust speed)
-      for (int j = 0; j < abs(control_output) / 100; j++) {
+    if(lat_pos > desired_lat_pos) {
+      for (int j = 0; j < 2; j++) {
         for (int i = 0; i < 11; i++) rs485.write(left[i]);
       }
     }
-  }
-}
-
-void move_long() {
-  int desired;
-  int dif = desired_pos_adc - current_pos_long;
-  if(dif > 55) { // AGV pos < desired
-    desired = desired_pos_adc - 20;
-  }
-  else if(dif > 30 && dif <= 55) { // AGV pos < desired
-    desired = desired_pos_adc - 15;
-    group = 1;
-  }
-  else if (dif < -30 && dif >= -55) { // AGV pos > desired
-    desired = desired_pos_adc + 15;
-    group = 1;
-  }
-  else if (dif < -55) { // AGV pos < desired
-    desired = desired_pos_adc + 20;
-  }
-  else {
-    desired = current_pos_long;
-  }
-
-  if(abs(current_pos_long - desired) > 5) {
-    if (current_pos_long > desired) {
-      for (int j = 0; j < group; j++) {
-        for (int i = 0; i < 11; i++) rs485.write(fwd[i]);
-      }
-    }
-    else if (current_pos_long < desired) {
-      for (int j = 0; j < group; j++) {
-        for (int i = 0; i < 11; i++) rs485.write(bkwd[i]);
-      }
-    }
-  }
-  else {
-    mv_long = false;
-    group = 2;
-  }
-}
-
-void manual() {
-  switch (commandState) {
-    case 0: // idle
-      for (int i = 0; i < 11; i++) rs485.write(idle[i]);
-      break;
-
-    case 1: // fwd
-      for (int j = 0; j < group; j++) {
-        for (int i = 0; i < 11; i++) rs485.write(fwd[i]);
-      }
-      commandState = 0;
-      break;
-
-    case 2: // bkwd
-      for (int j = 0; j < group; j++) {
-        for (int i = 0; i < 11; i++) rs485.write(bkwd[i]);
-      }
-      commandState = 0;
-      break;
-
-    case 3: // ccw
-      for (int j = 0; j < group; j++) {
-        for (int i = 0; i < 11; i++) rs485.write(ccw[i]);
-      }
-      commandState = 0;
-      break;
-
-    case 4: // cw
-      for (int j = 0; j < group; j++) {
-        for (int i = 0; i < 11; i++) rs485.write(cw[i]);
-      }
-      commandState = 0;
-      break;
-
-    case 5: // slower
-      for (int j = 0; j < group; j++) {
-        for (int i = 0; i < 11; i++) rs485.write(slow[i]);
-      }
-      commandState = 0;
-      break;
-
-    case 6: // faster
-      for (int j = 0; j < group; j++) {
-        for (int i = 0; i < 11; i++) rs485.write(fast[i]);
-      }
-      commandState = 0;
-      break;
-
-    case 7: // left
-      for (int j = 0; j < group; j++) {
-        for (int i = 0; i < 11; i++) rs485.write(left[i]);
-      }
-      commandState = 0;
-      break;
-
-    case 8: // right
-      for (int j = 0; j < group; j++) {
+    else if (lat_pos < desired_lat_pos) {
+      for (int j = 0; j < 2; j++) {
         for (int i = 0; i < 11; i++) rs485.write(right[i]);
       }
-      commandState = 0;
-      break;  
+    }
+  } 
+}
 
-    default:
+void set_cmd(int incomingByte) {
+  switch (incomingByte) {
+    case 119: // w, fwd
+      cmd_state = 1;
+      break;
+    
+    case 115: // s, bkwd
+      cmd_state = 2;
+      break;
+
+    case 97: // a, left
+      cmd_state = 3;
+      break;
+
+    case 100: // d, right
+      cmd_state = 4;
+      break;
+
+    case 113: // q, ccw
+      cmd_state = 5;
+      break;
+
+    case 101: // e, cw  
+      cmd_state = 6;
       break;
   }
 }
 
-int PID() {
-    int error = abs(sensor_0 - sensor_1);
-    P = Kp * error;
-    I += Ki * sample_t * (error + prev_e) * 0.5;
-    D = ((2 * Kd) / (sample_t + 2 * tau)) * (error - prev_e) - (
-          (sample_t - 2 * tau) / (sample_t + 2 * tau)) * D;
+void cntrl_speed() {
+  static unsigned long last_cmd_time = 0;  // Time of last command sent
 
-    prev_e = error;
-    int PID = P + I + D;
+  // Calculate PID output and desired speed change
+  float error = desired_lat_pos - lat_pos;
+  P = Kp * error;
+  I += Ki * sample_t * error;
+  D = (2 * Kd / (sample_t + 2 * tau)) * (error - prev_e) - ((sample_t - 2 * tau) / (sample_t + 2 * tau)) * D;
+  prev_e = error;
 
-    // anti-windup
-    if (PID > 500) PID = 500;
-    else if (PID < 0) PID = abs(PID);
+  // Normalize desired speed change to range [-1, 1] based on min and max speeds
+  float normalized_speed_change = (P + I + D) / (0.1 - 0.02);  // Adjust the denominator based on your min and max speed values
 
-    return PID/100;
+  // Constrain normalized speed change to [-1, 1]
+  normalized_speed_change = constrain(normalized_speed_change, -1.0, 1.0);
+
+  // Convert normalized speed change to speed steps (0 to 10)
+  int speed_steps = abs(round(normalized_speed_change * 10));  // Scale by 10 for 0-10 steps
+
+  // Send commands based on desired speed change and timing
+  if (millis() - last_cmd_time >= 20) {  // Check if 20ms have passed since last command
+    if (normalized_speed_change > 0) {
+      rs485.write(fast, sizeof(fast));  // Send "fast" for 20ms
+      last_cmd_time = millis();  // Update last command time
+    } else if (normalized_speed_change < 0) {
+      rs485.write(slow, sizeof(slow));  // Send "slow" for 20ms
+      last_cmd_time = millis();  // Update last command time
+    }
+  }
 }
